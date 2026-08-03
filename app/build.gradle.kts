3db1177ef2c3d2099747dc4044e895a8a75db471
anglesgirl <anglesgirl@users.noreply.github.com>
2026-08-03 16:34:59 +0000

    chore: 版本号升至 26.3.2(260804) ECH 修复版

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "io.github.daisukikaffuchino.han1meviewer"
         minSdk = 29
         targetSdk = 37
-        versionCode = 260803
-        versionName = "26.3.1"
+        versionCode = 260804
+        versionName = "26.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
